3cb0dfbe2bdb54aa61b9b08047e041d10635c10d
nenavatharuna <nenavatharuna8466@gmail.com>
2026-08-27 14:52:58 +0530

    Updated correct files

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,4 @@
+pipeline{
 stages {
         stage('Build') {
             steps {
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    student_id = request.form['student_id']
+    email = request.form['email']
+    year = request.form['year']
+    return render_template('successful.html', name=name, year=year)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
